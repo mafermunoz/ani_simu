5500df7f8bac487af55fd2cb64c0de8b6747edcd
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-06-14 02:13:43 +0800

    Update ani_simu.py

--- ani_simu.py
+++ ani_simu.py
@@ -71,13 +71,14 @@ for i,x in enumerate(poisson_dist[:100]):
     print(search_radius)
     for j in range (x): ## Loops over the given amount of measurements per second
         dummy2=np.random.randint(len_list)
-        aa=search_radius[search_radius==int(dipole_dist[dummy2,2])]
+        aa=search_radius[search_radius==dipole_dist[dummy2,2]]
         print()
         while (aa.size>0)==False:
             dummy2=np.random.randint(len_list)
             print('stuck')
             print(dipole_dist[dummy2,2])
-            aa=search_radius[search_radius==int(dipole_dist[dummy2,2])]
+            aa=search_radius[search_radius==ipole_dist[dummy2,2]]
+
         c1=SkyCoord(dipole_dist[dummy2,0]*u.deg,dipole_dist[dummy2,1]*u.deg,frame='fk5')
         offset=c2.spherical_offsets_to(c1)
         ra[ctracks]=dipole_dist[dummy2,0]
