785e9d653cb07a019f4ac9d5cc247a22215f1c6a
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-06-14 21:44:26 +0800

    Create submit_ani_simu.sh

---
+++ submit_ani_simu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=rhel6-long
+#SBATCH --ntasks=1
+#SBATCH --mem=30G
+#SBATCH --job-name=Ani
+
+
+export HOME=/atlas/users/mmunozsa/
+
+source /cvmfs/dampe.cern.ch/rhel6-64/etc/setup.sh
+source ~/astro/bin/activate
+
+
+python ani_simu.py
